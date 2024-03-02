712b291b2a14aaee43a160afc11a447e1ebd55da
soumya3969 <srmhnt2001@gmail.com>
2024-03-02 11:23:54 +0530

    assignment 3 incompleted

--- assignments/src/App.js
+++ assignments/src/App.js
@@ -1,10 +1,12 @@
 import BgcChanger from "./Components/BgcChanger";
 import CountNUpperCase from "./Components/CountNUpperCase";
+import ColorGenerator from "./Components/ColorGenerator";
 function App() {
   return (
     <div className="App">
       <BgcChanger />
       <CountNUpperCase/>
+      <ColorGenerator/>
     </div>
   );
 }
---
+++ assignments/src/Components/ColorGenerator.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const ColorGenerator = () => {
+    const [red, setRed] = useState(37);
+    const [green, setGreen] = useState(37);
+    const [blue, setBlue] = useState(37);
+
+    const updateColor = (e) => {
+        const value = Number(e.target.value);
+        const name = e.target.name;
+        if (value >= 0 && value <= 255) {
+            if (name === 'red') setRed(value);
+            if (name === 'green') setGreen(value);
+            if (name === 'blue') setBlue(value);
+        }else{
+            alert('⚠ Invalid Input please enter value in the range 0-255')
+        }
+    };
+
+    const bgColor =`rgb(${red}, ${green}, ${blue})`
+
+    return (
+        <div style={{
+            marginTop: '1rem',
+            display: 'grid',
+            placeItems: 'center'
+        }}>
+            <h1>Color Generator</h1>
+            <div>
+                <div>
+                    <label htmlFor="red">RED: </label>
+                    <input
+                        type="number"
+                        placeholder='0-255'
+                        name="red"
+                        onChange={updateColor}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="green">GREEN: </label>
+                    <input
+                        type="number"
+                        placeholder='0-255'
+                        name="green"
+                        onChange={updateColor}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="blue">BLUE: </label>
+                    <input
+                        type="number"
+                        placeholder='0-255'
+                        name="blue"
+                        onChange={updateColor}
+                    />
+                </div>
+                <div id='displayColor' style={{ width: '100px', height: '100px', background:{bgColor}}}></div>
+            </div>
+        </div>
+    );
+};
+
+export default ColorGenerator;
